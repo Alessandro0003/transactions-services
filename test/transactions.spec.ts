fe2eefded0b2b503be4a99ff967bbb95cc0e7009
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-13 16:04:38 -0300

    :test_tube: test: ensure Transaction routes to able to list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,14 +1,21 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
 describe('Transactions routes', () =>{
     beforeAll(async () => {
+
         await app.ready()
     })
     
     afterAll(async () => {
         await app.close()
+    })
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
     })
     
     it('should be able to create a new transactions ', async () => {
@@ -20,5 +27,32 @@ describe('Transactions routes', () =>{
                 type: 'credit'
             })
             .expect(201)
+
+            
     })
+
+    it('should be able to list all transaction',async  () => {
+        const createTransactionsResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New Transactions',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionsResponse.get('Set-Cookie')
+        
+        const listTransactionsResonse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            
+            .expect(200)
+        expect(listTransactionsResonse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New Transactions',
+                amount: 5000,
+            }),
+        ])
+    })  
+
 })
